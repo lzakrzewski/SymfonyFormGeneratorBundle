b785824201eb9ddf707cedf05803db99b5e4e3d8
lucaszz <lucaszz111@gmail.com>
2015-12-02 12:54:04 +0100

    Dependency injection added to bundle

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Lucaszz\SymfonyFormGeneratorBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /** {@inheritdoc} */
+    public function getConfigTreeBuilder()
+    {
+        //
+
+        $treeBuilder = new TreeBuilder();
+        $treeBuilder->root('symfony_form_generator');
+
+        return $treeBuilder;
+    }
+}
